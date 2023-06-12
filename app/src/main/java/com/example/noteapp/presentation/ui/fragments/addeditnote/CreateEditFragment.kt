22386b0a789cdbf922594b8119fbf0e6d0d2e433
HP <rasanlulov5@gmail.com>
2023-06-12 14:42:45 +0845

    дз3

--- app/src/main/java/com/example/noteapp/presentation/ui/fragments/addeditnote/CreateEditFragment.kt
+++ app/src/main/java/com/example/noteapp/presentation/ui/fragments/addeditnote/CreateEditFragment.kt
@@ -1,69 +1,56 @@
-package com.example.noteapp.presentation.ui.fragments.listofnotes
+package com.example.noteapp.presentation.ui.fragments.addeditnote
 
+import android.widget.Toast
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.a6monthlesson1.presentation.utils.UIState
-import com.example.noteapp.databinding.FragmentListOfNoteBinding
+import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentCreateEditBinding
+import com.example.noteapp.domain.model.Note
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class ListOfNoteFragment : Fragment() {
+class CreateEditFragment : Fragment() {
 
-    private lateinit var binding: FragmentListOfNoteBinding
-    private val viewModel by viewModels<ListOfNoteViewModel>()
+    private lateinit var binding: FragmentCreateEditBinding
+    private val viewModel by viewModels<CreateEditViewModel>()
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentListOfNoteBinding.inflate(inflater, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCreateEditBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        getAllNotes()
+        initClickers()
 
     }
 
-    private fun getAllNotes() {
-        viewModel.getAllNotes()
-        viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.getAllNotesState.collect { state ->
-                    when (state) {
-                        is UIState.Empty -> {}
-                        is UIState.Error -> {
-                            Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT)
-                                .show()
-                            Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                        is UIState.Loading -> {
-                            //show progress bar
-                        }
-                        is UIState.Success -> {
-                            //set list to adapter
-                        }
-                    }
-                }
-            }
+    private fun initClickers() {
+        binding.createEditBtn.setOnClickListener{
+            createNote(Note(title = binding.titleEt.text.toString(), desc = binding.descEt.text.toString()))
+            //editNote()
         }
     }
 
-    private fun removeNote(note: Note) {
-        viewModel.removeNote(note)
+    private fun createNote(note: Note) {
+        viewModel.createNote(note)
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.removeNoteState.collect { state ->
+                viewModel.createNoteState.collect { state ->
                     when (state) {
                         is UIState.Empty -> {}
                         is UIState.Error -> {
@@ -75,10 +62,36 @@ class ListOfNoteFragment : Fragment() {
                         }
 
                         is UIState.Success -> {
-                            //remove in  dao
+                            //add note to list
+
                         }
                     }
                 }
             }
         }
     }
+    private fun editNote(note: Note) {
+        viewModel.editNote(note)
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.editNoteState.collect { state ->
+                    when (state) {
+                        is UIState.Empty -> {}
+                        is UIState.Error -> {
+                            Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        is UIState.Loading -> {
+                            //show progress bar
+                        }
+
+                        is UIState.Success -> {
+                            //edit note
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/noteapp/presentation/ui/fragments/listofnotes/ListOfNoteViewModel.kt
+++ app/src/main/java/com/example/noteapp/presentation/ui/fragments/listofnotes/ListOfNoteViewModel.kt
@@ -25,6 +25,8 @@ class ListOfNoteViewModel @Inject constructor(
 
     private val _getAllNotesState = MutableStateFlow<UIState<List<Note>>>(UIState.Empty())
     val getAllNotesState = _getAllNotesState.asStateFlow()
+    private val _removeNoteState = MutableStateFlow<UIState<Unit>>(UIState.Empty())
+    val removeNoteState = _removeNoteState.asStateFlow()
     fun getAllNote() {
         viewModelScope.launch {
             getAllNotesUseCase.getAllNotes().collect() { res ->
@@ -43,6 +45,27 @@ class ListOfNoteViewModel @Inject constructor(
             }
         }
     }
+        fun removeNote(note: Note) {
+            viewModelScope.launch {
+                removeNoteUseCase.removeNote(note).collect() { res ->
+                    when (res) {
+                        is Resource.Error -> {
+                            _removeNoteState.value = UIState.Error(res.message!!)
+                        }
+
+                        is Resource.Loading -> {
+                            _removeNoteState.value = UIState.Loading()
+                        }
+
+                        is Resource.Success -> {
+                            if (res.data != null)
+                                _removeNoteState.value = UIState.Success(Unit)
+                        }
+                    }
+
+                }
+            }
+        }
 }
 
 
